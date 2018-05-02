4d6842ba4fe35baca9c0b8f9341514ac802f492e
imreFitos <imre.fitos@gmail.com>
2018-05-01 22:46:50 -0400

    initial commit

---
+++ csvtopostgresql.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+#
+# take a CSV file with a header and create a table and populate with all the data
+#
+# imre Fitos, 2018
+
+require 'csv'
+require 'pg'
+
+puts "#{$0} #{ARGV} started."
+
+unless ENV["DATABASE_URL"]
+  puts "#{$0} missing DATABASE_URL environment variable, aborted."; exit 1
+end
+
+url = ENV["DATABASE_URL"].split(/[\/:@]/)
+(puts "#{$0} missing db name from DATABASE_URL, aborted."; exit 1) unless dbname = url[6]
+(puts "#{$0} missing host name from DATABASE_URL, aborted."; exit 1) unless host = url[5]
+(puts "#{$0} missing username from DATABASE_URL, aborted."; exit 1) unless password = url[4]
+(puts "#{$0} missing password from DATABASE_URL, aborted."; exit 1) unless user = url[3]
+
+unless ARGV.length == 2
+  puts "usage: #{$0} [csv-file-to-upload] [table-to-create-and-upload-to]. aborted."; exit 1
+end
+
+unless File.exist?(ARGV[0])
+  puts "#{$0} Cannot find #{ARGV[0]}, aborted."; exit 1
+end
+
+begin
+  conn = PG.connect(:host => host, :user => user, :password => password, :dbname => dbname)
+  conn.exec "set client_min_messages = warning"
+  conn.exec "DROP TABLE IF EXISTS #{ARGV[1]}"
+
+  File.open(ARGV[0], 'r') do |file|
+    csv = CSV.new(file, headers: false)
+    headers = csv.shift
+
+    createtable = "CREATE TABLE #{ARGV[1]} ("
+    headers.each { |h|
+      createtable << "#{h} TEXT, "
+    }
+    createtable.chomp!(", ")
+    createtable << ");"
+
+    conn.exec createtable
+
+    enco = PG::TextEncoder::CopyRow.new
+    conn.copy_data "COPY #{ARGV[1]} FROM STDIN", enco do
+      while row = csv.shift
+        conn.put_copy_data row
+      end
+    end
+  end
+
+rescue PG::Error => e
+  puts e.message
+  puts "#{$0} aborted."
+  exit 1
+ensure
+  conn.close if conn
+end
+puts "#{$0} #{ARGV} finished."
